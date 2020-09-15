765089b976a86b786f8d52923de6daccb0afd432
SchwiftyQQ <SchwiftyQQ@github.com>
2020-09-15 15:06:08 +0300

    eddits 1

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player;
+    public float smoothness = 0.3f;
+
+    private Vector3 velocity = Vector3.zero;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = new Vector3();
+        pos.x = player.position.x;
+        pos.z = player.position.z;
+        pos.y = player.position.y;
+        transform.position = Vector3.SmoothDamp(transform.position, pos, ref velocity, smoothness);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Player : MonoBehaviour
+{
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.DOJump(new Vector3(transform.position.x + 1f, 1f, transform.position.z), 2f, 1, 0.3f, false);
+            
+        }
+    }
+}
